84a8888597598bf336c44afc567368acd31063a6
Shyam-Raghuwanshi <shyamraghuonec@gmail.com>
2026-01-17 22:47:52 +0530

    feat: implement project cards link icons

--- website/src/components/ProjectGridTemplate/index.js
+++ website/src/components/ProjectGridTemplate/index.js
@@ -137,32 +137,36 @@ export default function ProjectGrids() {
                                     <div>
                                         <div className={styles.topRow}>
                                             <h4 className={styles.projectName}>
-                                                {project.repository_url ? (
-                                                    <a
-                                                        href={
-                                                            project.repository_url
-                                                        }
-                                                        target={
-                                                            project.repository_url.startsWith(
-                                                                'http'
-                                                            )
-                                                                ? '_blank'
-                                                                : '_self'
-                                                        }
-                                                        rel='noopener noreferrer'
-                                                        className={
-                                                            styles.modalLinkUrl_break_word
-                                                        }
-                                                        onClick={(e) =>
-                                                            e.stopPropagation()
-                                                        }
-                                                    >
-                                                        {project.name}
-                                                    </a>
-                                                ) : (
-                                                    project.name
-                                                )}
+                                                {project.name}
                                             </h4>
+                                            {project.repository_url && (
+                                                <a
+                                                    href={project.repository_url}
+                                                    target='_blank'
+                                                    rel='noopener noreferrer'
+                                                    className={styles.externalLinkIcon}
+                                                    onClick={(e) =>
+                                                        e.stopPropagation()
+                                                    }
+                                                    title="View repository"
+                                                >
+                                                    <svg
+                                                        width="16"
+                                                        height="16"
+                                                        fill="currentColor"
+                                                        viewBox="0 0 16 16"
+                                                    >
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                                                        />
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                                                        />
+                                                    </svg>
+                                                </a>
+                                            )}
                                         </div>
                                     </div>
 
