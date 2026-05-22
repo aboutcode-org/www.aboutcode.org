95a5d1b55284741c437d9c5511b50cad17f0acd1
Philippe Ombredanne <pombredanne@aboutcode.org>
2026-05-22 23:34:05 +0200

    Improve hero and tag line
    Signed-off-by: Philippe Ombredanne <pombredanne@aboutcode.org>

--- website/src/components/HomepageHeader/index.js
+++ website/src/components/HomepageHeader/index.js
@@ -6,11 +6,10 @@ export default function HomepageHeader() {
     <div className={styles.fullHero}>
       <div className={styles.heroInner}>
         <h1 className={styles.heroTitle}>
-          Healthy and Safe<br />
-          Software Supply Chains
+          Healthy Software Supply Chains
         </h1>
         <p className={styles.heroSubtitle}>
-          We build, maintain, and sustain open data with open source code and open standards.
+          We build, maintain, and sustain open data with open source code and open standards for the software supply chains.
         </p>
       </div>
     </div>
