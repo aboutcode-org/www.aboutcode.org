53f8017874bcea25970f0dc5c34745b868e89ea8
Philippe Ombredanne <pombredanne@aboutcode.org>
2026-05-22 11:03:29 +0200

    Add new content to homepage
    Signed-off-by: Philippe Ombredanne <pombredanne@aboutcode.org>

--- website/src/components/HomepageContent/index.js
+++ website/src/components/HomepageContent/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import HomeIntro from './HomeIntro.mdx';
 import StandardsOverview from './StandardsOverview.md';
-import Adopters from './Adopters.md';
+import AdoptersBanner from '@site/src/components/AdoptersBanner';
 import ProjectOverview from './ProjectOverview.md';
 import Supporters from './Supporters.md';
+import ValuePillars from '@site/src/components/ValuePillars';
 import CapabilitiesGrid from '@site/src/components/CapabilitiesGrid';
+import EcosystemGrid from '@site/src/components/EcosystemGrid';
 import ProjectGridTemplate from '@site/src/components/ProjectGridTemplate';
 import styles from './styles.module.css';
 
@@ -24,6 +26,10 @@ export default function HomepageContent() {
                 <div className={styles.sectionIntro}>
                     <HomeIntro />
                 </div>
+            </section>
+
+            <section className={styles.sectionContainer}>
+                <ValuePillars />
             </section>
 
             <section className={styles.sectionContainer}>
@@ -49,9 +55,7 @@ export default function HomepageContent() {
                 >
                     <h2>Adopters</h2>
                 </div>
-                <div className={styles.sectionIntro}>
-                    <Adopters />
-                </div>
+                <AdoptersBanner />
             </section>
 
             <section className={styles.sectionContainer}>
@@ -65,6 +69,21 @@ export default function HomepageContent() {
                     <ProjectOverview />
                 </div>
                 <ProjectGridTemplate />
+            </section>
+
+            <section className={styles.sectionContainer}>
+                <div
+                    className={styles.sectionHeader}
+                    style={{ marginBottom: '15px', marginTop: '15px' }}
+                >
+                    <h2>Supported ecosystems and languages</h2>
+                </div>
+                <div className={styles.sectionIntro}>
+                    <p>AboutCode tools support 50+ package ecosystems, programming languages,
+                    and vulnerability data sources, all identified using{' '}
+                    <a href="https://packageurl.org">Package-URL (PURL)</a>.</p>
+                </div>
+                <EcosystemGrid />
             </section>
 
             <section className={styles.sectionContainer}>
