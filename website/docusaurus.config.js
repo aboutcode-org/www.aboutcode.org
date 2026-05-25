f0c8f24b97919e7209a26096c8f82aa276184529
Philippe Ombredanne <pombredanne@aboutcode.org>
2026-05-25 11:47:10 +0200

    Remove reference to docusaurus future
    I was playing with a docusaurus bump locally. This does not play
    well with the current version
    
    Signed-off-by: Philippe Ombredanne <pombredanne@aboutcode.org>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -52,7 +52,6 @@ const config = {
     // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
     future: {
         v4: true, // Improve compatibility with the upcoming Docusaurus v4
-        faster: false,
     },
 
     // Adapt the www.packageurl.org approach for the GH Pages vs. DreamHost baseUrl adjustment.
